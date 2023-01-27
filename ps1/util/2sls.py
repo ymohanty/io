8ba4b2bd03f088fe62d171dac2460975436e3497
briancurran29 <bcurran@uchicago.edu>
2023-01-26 23:19:18 -0600

    Made 2SLS estimator

---
+++ ps1/util/2sls.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Function to compute 2SLS estimator
+def tsls_estimator(X, Z, Y):
+    # Make transposes
+    XT = np.transpose(X)
+    ZT = np.transpose(Z)
+
+    # Make estimator
+    first_part = np.linalg.inv(XT @ Z @ np.linalg.inv(ZT @ Z) @ ZT @ X)
+    second_part = XT @ Z @ np.linalg.inv(ZT @ Z) @ ZT @ Y
+    estimator = np.matmul(first_part, second_part)
+
+    return estimator
