dd28fff149943bcffc37768d16117b36521a1858
Yashaswi Mohanty <josh.mohanty@gmail.com>
2023-03-03 09:56:43 -0600

    Add data to IO project

---
+++ ps3/ps3_q3.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import scipy
+
+class bidModel:
+
+    def __init__(self):
+
+        # Data
+        self.bids == []  #  2 x I matrix of bids
+        self.pred_part = [] # List of predicted participants
+        self.X = 
