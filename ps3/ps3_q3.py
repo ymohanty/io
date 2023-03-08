144b39ecbc6127a705e99145378f4e270825d046
Yashaswi Mohanty <josh.mohanty@gmail.com>
2023-03-08 17:56:18 -0600

    Implement lasso and regression framework to recover gamma

--- ps3/ps3_q3.py
+++ ps3/ps3_q3.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import numpy as np
 from scipy import sparse
+from sklearn import preprocessing
 from sklearn import linear_model
 from sklearn import metrics
+from sklearn import neural_network
 
 
 class BidModel:
 
     def __init__(self):
+        """
 
+        """
         # Data
         self.bid_data = []  # (3 x B) Pandas frame of bid characteristics
         self.num_pcpt = []  # (2 x A) Pandas frame of num. participants
@@ -16,13 +20,17 @@ class BidModel:
 
         # Homogenized bids
         self.model = []          # Scitkit learn/keras model predicted bids
-        self.homog_log_bids = [] #
+        self.homog = []          # Log homogenized bids
+        self.mse = np.Inf        # Model prediction error
 
     def get_data(self):
+        """
+
+        """
         # Get the bids
-        raw_bids = pd.read_csv('data/bids.csv')
-        raw_bids['log_bid'] = np.log(raw_bids['bid_value'])
-        self.bid_data = raw_bids
+        self.bid_data = pd.read_csv('data/bids.csv')
+        self.bid_data['log_bid'] = np.log(self.bid_data['bid_value'])
+        self.bid_data['id'] = self.bid_data.groupby(['item_num']).ngroup()
 
         # Get the auction participants
         self.num_pcpt = pd.read_csv('data/items.csv')
@@ -36,6 +44,9 @@ class BidModel:
         self.X = sparse.csr_matrix((data, (row, col)))
 
     def summarize_data(self):
+        """
+
+        """
         # Summary table
         bids_summary = pd.DataFrame(
             {
@@ -62,6 +73,7 @@ class BidModel:
         # Set the index to describe variable
         # summary.set_index(['Log-bids', 'Num. participants'])
         print(summary)
+        print("\n\n")
 
         # Output
         summary.to_latex(
@@ -77,13 +89,37 @@ class BidModel:
         )
 
     def homogenize_bids(self, method='ols'):
+        """
 
+        :param method:
+        """
+        y = self.bid_data['log_bid'].to_numpy()
+        X = self.X.todense()[self.bid_data['id'],:]
+        scaler = preprocessing.StandardScaler().fit(X)
+        X_scaled = scaler.transform(X)
         if method == 'ols':
-            self.model = linear_model.LinearRegression().fit(y,X)
+            print("Estimating gamma via least squares...")
+            y = y[..., None]
+            self.model = linear_model.LinearRegression().fit(X,y)
+            yhat = self.model.predict(X)
+            self.homog = y - yhat
+            self.mse = metrics.mean_squared_error(y,yhat)
+            print(f'Mean squared error (OLS): {self.mse:.2f}\n')
         elif method == 'lasso':
-            pass
+            print("Estimating gamma via lasso...")
+            cv = 5
+            self.model = linear_model.LassoCV(cv=cv,verbose=True, fit_intercept=True).fit(X_scaled,y)
+            yhat = self.model.predict(X_scaled)
+            self.homog = y - yhat
+            self.mse = metrics.mean_squared_error(y,yhat)
+            print(f'Number of cross-validation folds: {cv}')
+            print(f'Penalty: {self.model.alpha_:.3f}')
+            print(f'Mean squared error (Lasso): {self.mse:.2f}\n')
+
         elif method == 'neural':
-            pass
+            print("Estimating gamma via neural network...")
+            self.model = neural_network.MLPRegressor()
+
         else:
             raise ValueError(f"Method not recognized: {method}")
 
@@ -103,4 +139,11 @@ if __name__ == '__main__':
     # Part 4 -- See text
 
     # Part 5 -- See text
+
+    # Part 6
+    # (a)
     model.homogenize_bids('ols')
+
+    # (b)
+    model.homogenize_bids('lasso')
+
